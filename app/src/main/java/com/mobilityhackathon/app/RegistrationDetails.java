78d9d5b0bba3ebd1edd1863b7d5d0f57e822ce0b
Dmitry Dolgopolov <dimafet001@gmail.com>
2019-07-20 18:35:05 -0700

    Started adding smartcar

--- app/src/main/java/com/mobilityhackathon/app/RegistrationDetails.java
+++ app/src/main/java/com/mobilityhackathon/app/RegistrationDetails.java
@@ -13,8 +13,6 @@ public class RegistrationDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_details);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
     }
 
